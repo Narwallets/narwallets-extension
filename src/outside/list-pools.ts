8e2c206574c3d29bd004c08cb81c72cc51378f01
fercargo <fercargo@protonmail.com>
2022-10-09 11:21:25 -0300

    better list pools

--- src/outside/list-pools.ts
+++ src/outside/list-pools.ts
@@ -2,7 +2,7 @@ import * as d from "../util/document.js"
 import * as c from "../util/conversions.js"
 import * as StakingPool from "../contracts/staking-pool.js"
 
-import { askBackgroundGetValidators } from "../askBackground.js"
+import { askBackgroundGetNetworkInfo, askBackgroundGetValidators } from "../askBackground.js"
 import { activeNetworkInfo } from "../askBackground.js"
 
 
@@ -14,11 +14,6 @@ type PoolInfo = {
   stakeY: string;
   uptime: number;
   fee?: number;
-}
-
-function sortCompare(a: PoolInfo, b: PoolInfo) {
-  if (a.stakeY > b.stakeY) return -1;
-  return 1;
 }
 
 
@@ -46,12 +41,13 @@ async function displayStakingPools() {
         slashed: item.is_slashed ? "yes" : "no",
         stake: c.ytonString(item.stake),
         stakeY: item.stake.padStart(50, "0"),
-        uptime: Math.round(item.num_produced_blocks / item.num_expected_blocks * 100)
+        uptime: item.num_expected_blocks==0?0: Math.round(item.num_produced_blocks / item.num_expected_blocks * 100)
       })
 
     }
 
-    list.sort(sortCompare);
+    // sort desc by uptime
+    list.sort((a: PoolInfo, b: PoolInfo) => b.uptime - a.uptime);
 
     const TEMPLATE = `
     <li id="{name}">
@@ -68,36 +64,39 @@ async function displayStakingPools() {
     d.populateUL("list", TEMPLATE, list)
 
     //query fees
-    for (let item of data.current_validators) {
-      StakingPool.getFee(item.account_id) //async get fees
+    for (let item of list) {
+      StakingPool.getFee(item.name) //async get fees
         .then((fee) => {
           //debug
-          // if (item.account_id.indexOf("node0") != -1) {
+          // if (item.name.indexOf("node0") != -1) {
           //   //console.log(fee)
           // }
-          const elem = d.byId(item.account_id)
-          if (fee >= 50) elem.classList.add("hidden") //bye bye 
+          const elem = d.byId(item.name)
 
-          const feeSpan = d.byId(item.account_id + "-fee")
-          feeSpan.innerText = fee.toString();
-          const feeBox = feeSpan.parentElement;
-          if (feeBox) {
-            if (fee >= 30) feeBox.classList.add("red")
-            if (fee >= 5) feeBox.classList.add("yellow")
-            if (fee <= 5 && fee >= 0.5) feeBox.classList.add("green")
-          }
-          if (fee > 0 && fee < 50) {
-            const button: HTMLElement | null = elem.querySelector("button")
-            if (button) {
-              button.addEventListener("click", () => { clicked(item.account_id) })
-              button.classList.remove("hidden");
+          if (item.uptime >= 90) {
+            // do not colorate low-perf nodes
+            const feeSpan = d.byId(item.name + "-fee")
+            feeSpan.innerText = fee.toString();
+            const feeBox = feeSpan.parentElement;
+            if (feeBox) {
+              if (fee >= 30) feeBox.classList.add("red")
+              else if (fee > 10) feeBox.classList.add("orange")
+              else if (fee >= 5) feeBox.classList.add("yellow")
+              else feeBox.classList.add("green")
+            }
+            if (fee > 0 && fee < 50) {
+              const button: HTMLElement | null = elem.querySelector("button")
+              if (button) {
+                button.addEventListener("click", () => { clicked(item.name) })
+                button.classList.remove("hidden");
+              }
             }
           }
         })
         .catch((ex) => {
           console.log(ex)
           //no contract on account_id
-          const elem = d.byId(item.account_id)
+          const elem = d.byId(item.name)
           elem.classList.add("hidden") //bye bye 
         })
     }
@@ -116,7 +115,7 @@ async function displayStakingPools() {
 // ---------------------
 async function init() {
   try {
-    //rpc.addHeader("mode","no-cors")
+    await askBackgroundGetNetworkInfo();
     displayStakingPools();
   }
   catch (ex) {
