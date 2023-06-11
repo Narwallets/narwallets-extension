964858041b42c2452fcc223c123d092ce85d1a58
the-preem-palver <the.Preem.Palver@protonmail.com>
2023-06-10 22:02:00 -0300

    compile before publish

--- makezip.sh
+++ makezip.sh
@@ -4,8 +4,8 @@ echo "Remember: BUMP VERSION"
 read -p "Press Enter to continue..."
 
 rm out/extension.zip
-# rm -rf extension/dist
-# tsc -b --verbose
+rm -rf extension/dist
+tsc -b --verbose
 mkdir -p out
 cd extension
 zip -r ../out/extension.zip . -x '*.map'
